af3a24717abbd77a0bbb8b7c704568cfb5eb1aa4
User <User@DESKTOP-R0BLQ2C>
2021-05-02 15:54:59 +0300

    change some styles in admin panel

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -11,6 +11,7 @@ const AdminPanel = () => {
             setParamList(await fetchParams(type))
         }
         fetchData()
+        console.log(paramList)
     }, [type])
 
     const types = [
@@ -21,36 +22,65 @@ const AdminPanel = () => {
         {
             title: 'Engine Type',
             path: 'shopEngineType'
+        },
+        {
+            title: 'Marks',
+            path: 'shopMark'
+        },
+        {
+            title: 'Transmission',
+            path: 'shopTransmissionType'
+        },
+        {
+            title: 'Drive types',
+            path: 'shopDriveType'
         }
-
     ]
 
     return (
         <div className="ap__body">
             <div className="ap__content">
                 <div className="ap__types">
+
                     {types.map(el => {
-                        return <div onClick={() => setType(el.path)}>{el.title}</div>
+                        return <div className="ap__type_element" onClick={() => setType(el.path)}><div className="ap_type_text">{el.title}</div></div>
                     })}
                 </div>
                 <div className="ap__var">
+                    <div className="label_for_records">
+                        <div className="label_text">Add new record</div>
+                        <div className="manage_image_container op">
+                            <img className="manage_image" src="/Pictures/add.png" alt="" />
+                        </div>
+                    </div>
                     {paramList.map(el => {
-                        return <div style={{
-                            
-                        }}>
-                            <div>
-                                {el.type}
-                            </div>
-                            <div>
-                                <div>!</div> 
+                        return <div className="record">
+                            {Object.keys(el).map((field) => {
+                                return <div className="record_info">
+                                    {el[field]}
+                                </div>
+                            })
 
-                                <div>X</div>
+                            }
+
+
+
+
+                            <div className="record_manager_buttons">
+                                <div className="manage_image_container">
+                                    <img className="manage_image" src="/Pictures/Edit.png" alt="" />
+                                </div>
+
+                                <div className="manage_image_container">
+                                    <img className="manage_image" src="/Pictures/delete.png" alt="" />
+                                </div>
                             </div>
                         </div>
+
                     })}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
